951c725ca0eaebdbd344dc8b09500d31e5fc0282
Eran Ben-Horin <you@example.com>
2020-11-12 15:29:49 +0200

    parse epoch manually for format string

--- redis-6.0.9/src/modules/systemtime.c
+++ redis-6.0.9/src/modules/systemtime.c
@@ -9,6 +9,7 @@
 static int setup_io_multiplexer();
 static int setup_timer();
 static int start_sys_time_collector_thread();
+static void convert_time_to_human_string(time_t seconds, char* buffer);
 
 static int s_epoll_fd = 0;
 static int s_timer_fd = 0;
@@ -57,9 +58,11 @@ int DrivenetsGetTime_RedisCommand(RedisModuleCtx *ctx, RedisModuleString **argv,
 #endif
 
 	///// send the result back.
-#ifdef __USE_HUMAN_TIME	
+#ifdef __USE_HUMAN_TIME
+	char buffer [128] = {0};
 	time_t time = system_time_usec / NANO_IN_SEC;
-	RedisModule_ReplyWithCString(ctx, asctime(localtime(&time)));
+	convert_time_to_human_string(time, buffer);
+	RedisModule_ReplyWithCString(ctx, buffer);
 #else
 	RedisModule_ReplyWithLongLong(ctx, system_time_usec);
 #endif
@@ -247,4 +250,56 @@ int start_sys_time_collector_thread()
     }
     // TODO where do i join with this?
     return 0;
+}
+
+void convert_time_to_human_string(time_t time, char* buffer)
+{
+	uint32_t seconds, minutes, hours, days, year, month;
+
+	seconds = time;
+
+	/* calculate minutes */
+	minutes  = seconds / 60;
+	seconds -= minutes * 60;
+	/* calculate hours */
+	hours    = minutes / 60;
+	minutes -= hours   * 60;
+	/* calculate days */
+	days     = hours   / 24;
+	hours   -= days    * 24;
+
+	/* Unix time starts in 1970 on a Thursday */
+	year      = 1970;
+
+	while(1)
+	{
+		int leapYear   = (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0));
+		uint16_t daysInYear = leapYear ? 366 : 365;
+		if (days >= daysInYear)
+		{
+			days -= daysInYear;
+			++year;
+		}
+		else
+		{
+			/* calculate the month and day */
+			static const uint8_t daysInMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+			for(month = 0; month < 12; ++month)
+			{
+				uint8_t dim = daysInMonth[month];
+
+				/* add a day to feburary if this is a leap year */
+				if (month == 1 && leapYear)
+					++dim;
+
+				if (days >= dim)
+					days -= dim;
+				else
+					break;
+			}
+			break;
+		}
+	}
+
+	sprintf(buffer, "%d-%d-%d %d:%d:%d", days+1, month+1, year, hours+2, minutes, seconds);
 }
