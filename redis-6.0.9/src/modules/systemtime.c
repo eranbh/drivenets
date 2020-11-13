cd252cbbdda87a6669b0286e0b382f5045ead565
Eran Ben-Horin <you@example.com>
2020-11-13 16:28:39 +0200

    code review comments
    1. added redis based logging.
    2. changed the sync facility in atomics case from cas to fetch_add
    3. added more fine grained error handling to the multiplexer

--- redis-6.0.9/src/modules/systemtime.c
+++ redis-6.0.9/src/modules/systemtime.c
@@ -6,9 +6,9 @@
 #include <errno.h>
 #include "../redismodule.h"
 
-static int setup_io_multiplexer();
-static int setup_timer();
-static int start_sys_time_collector_thread();
+static int setup_io_multiplexer(RedisModuleCtx *ctx);
+static int setup_timer(RedisModuleCtx *ctx);
+static int start_sys_time_collector_thread(RedisModuleCtx *ctx);
 static void convert_time_to_human_string(time_t seconds, char* buffer);
 
 static int s_epoll_fd = 0;
@@ -78,33 +78,34 @@ int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) 
     // means we are "open for business" which is not true yet
 
     // setup io multiplexer
-    if (0 != setup_io_multiplexer()){
-		printf("setting up the io multiplexer failed.\n");
+    if (0 != setup_io_multiplexer(ctx)){
+		RedisModule_Log(ctx, "error", "setting up the io multiplexer failed");
         return REDISMODULE_ERR;
     }
 
     // setup time and timer related stuff
     // note: timer is _not_ armed here!
-    if (0 != setup_timer()){
-        printf("setting up timer related stuff failed.\n");
+    if (0 != setup_timer(ctx)){
+		RedisModule_Log(ctx, "error", "setting up timer related stuff failed");
         return REDISMODULE_ERR;
     }
 
 #ifdef __USE_MUTEX
-	if (pthread_mutex_init(&lock, NULL) != 0) { 
+	if (pthread_mutex_init(&lock, NULL) != 0) {
+		RedisModule_Log(ctx, "error", "pthread_mutex_init failed");
         return REDISMODULE_ERR; 
     }
 #endif
 
 
     // start the collector thread
-    if (0 > start_sys_time_collector_thread()){
-        printf("starting the collector thread failed.\n");
+    if (0 > start_sys_time_collector_thread(ctx)){
+		RedisModule_Log(ctx, "error", "starting the collector thread failed");
         return REDISMODULE_ERR;
     }
 
     if (RedisModule_Init(ctx,"drivenetsgettime",1,REDISMODULE_APIVER_1) == REDISMODULE_ERR){
-        printf("RedisModule_Init failed.\n");
+		RedisModule_Log(ctx, "error", "RedisModule_Init failed");
 		return REDISMODULE_ERR;
 	}
 
@@ -113,19 +114,19 @@ int RedisModule_OnLoad(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) 
                                   DrivenetsGetTime_RedisCommand,
 								  "readonly",0,0,0)              == REDISMODULE_ERR){
 
-        printf("DrivenetsGetTime_RedisCommand failed.\n");
+		RedisModule_Log(ctx, "error", "DrivenetsGetTime_RedisCommand failed");
         return REDISMODULE_ERR;
 	}
 
 	return REDISMODULE_OK;
 }
 
-int setup_io_multiplexer()
+int setup_io_multiplexer(RedisModuleCtx *ctx)
 {
     // we want a multiplexer of size one.
     // just one timeout to manage
     if (0 > (s_epoll_fd = epoll_create(1))){
-		printf ("epoll_create failed. errno: %d\n", errno);
+		RedisModule_Log(ctx, "error", "epoll_create failed. errno: %d", errno);
 		return -1;
 	}
  
@@ -136,7 +137,8 @@ int setup_io_multiplexer()
     // on the read when we leave the epoll on a _non_ timer related event
 	s_timer_fd = timerfd_create(CLOCK_MONOTONIC, TFD_CLOEXEC | TFD_NONBLOCK);
 	if (0 > s_timer_fd){
-		printf ("timerfd_create failed. errno: %d\n", errno);
+		RedisModule_Log(ctx, "error", "timerfd_create failed. errno: %d", errno);
+		return -1;
 	}
 	
     // add the timer to the multiplexer
@@ -145,13 +147,13 @@ int setup_io_multiplexer()
 	ev.data.fd = s_timer_fd;
 	int sts = epoll_ctl(s_epoll_fd, EPOLL_CTL_ADD, s_timer_fd, &ev);
 	if (0 > sts){
-		printf ("epoll_ctl failed. errno: %d\n", errno);
+		RedisModule_Log(ctx, "error", "epoll_ctl failed. errno: %d", errno);
 		return -1;
 	}
 	return 0;
 }
 
-int setup_timer()
+int setup_timer(RedisModuleCtx *ctx)
 {
     struct itimerspec its;
 	// according to time(7) the accuracy of systemcalls that set timers
@@ -161,7 +163,7 @@ int setup_timer()
 	// take this constant from the system as it is coupled to the os / hw versions
 	long hz = sysconf(_SC_CLK_TCK);
 	if (0 > hz){
-		printf ("sysconf failed. errno: %d\n", errno);
+		RedisModule_Log(ctx, "error", "sysconf failed. errno: %d", errno);
 		return -1;
 	}
 	// one jiffi in seconds, is therefor:
@@ -173,20 +175,22 @@ int setup_timer()
 	its.it_interval.tv_nsec = jiffi_in_seconds * NANO_IN_SEC;
 	its.it_value.tv_sec = 1; // we start aligned to a full second
 	its.it_value.tv_nsec = 0;
-	if (timerfd_settime(s_timer_fd, 0, &its, NULL) < 0)
+	if (timerfd_settime(s_timer_fd, 0, &its, NULL) < 0){
+		RedisModule_Log(ctx, "error", "timerfd_settime failed. errno: %d", errno);
 		return -1;
+	}
 	return 0;
 }
 
 void* collector_thread_work(void* user)
 {
-	REDISMODULE_NOT_USED(user);
+	RedisModuleCtx *ctx = (RedisModuleCtx*) user;
 	struct epoll_event events[1] = {0};
 	// thread starting. take current time
 	struct timespec ts;
 	int sts = clock_gettime(CLOCK_REALTIME, &ts);
 	if (-1 == sts){
-		printf("clock_gettime(CLOCK_REALTIME) failed\n");
+		RedisModule_Log(ctx, "error", "clock_gettime(CLOCK_REALTIME) failed");
 		return (void*)-1;
 	}
 	
@@ -211,28 +215,31 @@ void* collector_thread_work(void* user)
 			uint64_t times_expired;
 			ssize_t size = read(events[0].data.fd, &times_expired, sizeof(uint64_t));
 			if(size != sizeof(uint64_t)) {
-				printf("read error. errno: %d\n", errno);
-				// TODO if not EAGAIN, break the loop
+				// the man says not reading a full uint64_t is an error
+				// report an error and terminate, unless its these cases
+				if(errno != EAGAIN && errno != EINTR){
+					RedisModule_Log(ctx, "error", "read error. errno: %d", errno);
+					return (void*)-1;
+				}else{
+					// spurious wakeup of some sort ... retry
+					continue;
+				}
 			}
 #ifdef __USE_MUTEX
 			pthread_mutex_lock(&lock);
 			s_system_time_usec += times_expired * (jiffi_in_seconds * NANO_IN_SEC);
 			pthread_mutex_unlock(&lock);
-#elif __USE_ATOMICS
-			// this is a tricky one. we need CAS to do this
-			long system_time_usec = 0;
-			long new_sys_time = 0;
-			do{
-				new_sys_time = system_time_usec = s_system_time_usec;
-				new_sys_time += times_expired * (jiffi_in_seconds * NANO_IN_SEC);
-			}while (0 == __sync_bool_compare_and_swap(&s_system_time_usec,
-			                                          system_time_usec,
-													  new_sys_time));
+#elif __USE_ATOMICS			
+			int delta = times_expired * (jiffi_in_seconds * NANO_IN_SEC);
+			__sync_add_and_fetch(&s_system_time_usec, delta);
 #endif
 		}
 		else{
-			printf("no events happened. stopping multiplexer\n");
-			return (void*)-1;
+			// epoll_wait failed. only EINTR is recoverable
+			if(errno != EINTR){
+				RedisModule_Log(ctx, "error", "no events happened. stopping multiplexer. errno: %d", errno);
+				return (void*)-1;
+			}
 		}
 	}
 	return (void*)0;
@@ -240,12 +247,12 @@ void* collector_thread_work(void* user)
 
 
 
-int start_sys_time_collector_thread()
+int start_sys_time_collector_thread(RedisModuleCtx *ctx)
 {
     pthread_t threadId = -1;
-    int sts = pthread_create(&threadId, NULL, &collector_thread_work, 0);
+    int sts = pthread_create(&threadId, NULL, &collector_thread_work, (void*)ctx);
     if (0 > sts){
-	    printf ("pthread_create failed. errno: %d\n", errno);
+		RedisModule_Log(ctx, "error", "pthread_create failed. errno: %d", errno);
 	    return -1;
     }
     // TODO where do i join with this?
